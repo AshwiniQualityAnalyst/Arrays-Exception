b2deab9f6368760ec3e6c8bcb3076d34a1aeaf72
AshwiniQualityAnalyst <ashwini.qualityanalyst@gmail.com>
2018-11-25 21:51:21 +0530

    Update FinalizeMethod.java

--- src/Exceptions/FinalizeMethod.java
+++ src/Exceptions/FinalizeMethod.java
@@ -20,4 +20,5 @@ public class FinalizeMethod extends GregorianCalendar {
 
 // final and finally are keywords in java whereas finalize is a method
 // final keyword can be used with class variables so that they can’t be reassigned, with class to avoid extending by classes and with methods to avoid overriding by subclasses
-// 
+// finally keyword is used with try-catch block to provide statements that will always gets executed even if some exception arises, usually finally is used to close resources.
+// finalize() method is executed by Garbage Collector before the object is destroyed, it’s great way to make sure all the global resources are closed.
