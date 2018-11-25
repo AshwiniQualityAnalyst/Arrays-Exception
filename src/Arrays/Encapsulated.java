3773ecea2060b62bafc1bbc468be2c2a9f9466e3
AshwiniQualityAnalyst <ashwini.qualityanalyst@gmail.com>
2018-11-25 22:12:08 +0530

    Update Encapsulated.java

--- src/Arrays/Encapsulated.java
+++ src/Arrays/Encapsulated.java
@@ -1,25 +1,57 @@
+//the variables or data of a class is hidden from any other class and can be accessed only through any member function of own class
+
+//in which they are declared.
+
+//As in encapsulation, the data in a class is hidden from other classes, so it is also known as data-hiding.
+
+//Encapsulation can be achieved by: Declaring all the variables in the class as private and writing public methods in the
+
+//class to set and get the values of variables.
+
 package Arrays;
 
-class Encapsulated 
-{
-	private String name;
+class Encapsulate {
 
-	public String getName()
-	{
-		return name;
+	private int Age;
+	private String Name;
+	private int Roll;
+
+	public int getAge() {
+		return Age;
 	}
 
-	public void setName(String name)
-	{
-		this.name = name;
+	public void setAge(int Age) {
+		this.Age = Age;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String Name) {
+		this.Name = Name;
+	}
+
+	public int getRoll() {
+		return Roll;
+	}
+
+	public void setRoll(int Roll) {
+		this.Roll = Roll;
 	}
 }
 
-class Test
-{
+public class Encapsulation {
+
 	public static void main(String[] args) {
-		Encapsulated e = new Encapsulated();
-		e.setName("Ashwini");
-		System.out.println(e.getName());
+		Encapsulate Object = new Encapsulate();
+		Object.setName("Ashwini");
+		System.out.println(Object.getName());
+
+		Object.setAge(30);
+		System.out.println(Object.getAge());
+
+		Object.setRoll(10645280);
+		System.out.println(Object.getRoll());
 	}
 }
